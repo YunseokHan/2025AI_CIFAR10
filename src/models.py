35bcd6057a9f852b785757b2083dcb38ce415664
YunseokHan <qicher@snu.ac.kr>
2025-05-04 09:00:47 +0000

    Add resnet50

--- src/models.py
+++ src/models.py
@@ -2,7 +2,14 @@ import torchvision.models as models
 import torch.nn as nn
 
 def get_model(name='resnet18'):
-    model = models.resnet18(weights=None, num_classes=10)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-    model.maxpool = nn.Identity()
+    if name == 'resnet18':
+        model = models.resnet18(weights='DEFAULT', num_classes=10)
+        model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        model.maxpool = nn.Identity()
+    
+    if name == 'resnet50':
+        model = models.resnet50(weights='DEFAULT', num_classes=10)
+        model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        model.maxpool = nn.Identity()
+    
     return model
